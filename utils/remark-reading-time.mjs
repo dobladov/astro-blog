3a1f2bce6a41b664247c673696012dd0a6f253b2
Daniel Doblado <danieldoblado@gmail.com>
2023-08-05 20:39:32 +0200

    Remove log

--- utils/remark-reading-time.mjs
+++ utils/remark-reading-time.mjs
@@ -3,11 +3,9 @@ import { toString } from 'mdast-util-to-string'
 
 export const remarkReadingTime = () => {
   return (tree, { data }) => {
-    console.log(tree)
     const textOnPage = toString(tree)
     const readingTime = getReadingTime(textOnPage)
 
-    console.log(readingTime)
     data.astro.frontmatter.minutesRead = readingTime.text
     data.astro.frontmatter.words = readingTime.words
   }
