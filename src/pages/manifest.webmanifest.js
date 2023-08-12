f65c9db46ffbc9784d3f5d5b7f618202b6c35660
Daniel Doblado <danieldoblado@gmail.com>
2023-08-12 20:40:01 +0200

    Optimize public assets

--- src/pages/manifest.webmanifest.js
+++ src/pages/manifest.webmanifest.js
@@ -19,45 +19,39 @@ export async function get () {
       lang: 'en-US',
       start_url: '/',
       display: 'minimal-ui',
-      background_color: 'hsl(0, 0%, 95%)',
-      theme_color: 'hsl(180, 3%, 23%)',
+      background_color: '#18181b',
+      theme_color: 'rgb(161, 161, 170)',
       description: SITE_DESCRIPTION,
       icons: [
         {
-          src: 'images/touch/homescreen48.png',
+          src: '/orb48.png',
           sizes: '48x48',
           type: 'image/png'
         },
         {
-          src: 'images/touch/homescreen72.png',
+          src: '/orb72.png',
           sizes: '72x72',
           type: 'image/png'
         },
         {
-          src: 'images/touch/homescreen96.png',
+          src: '/orb96.png',
           sizes: '96x96',
           type: 'image/png'
         },
         {
-          src: 'images/touch/homescreen144.png',
+          src: '/orb144.png',
           sizes: '144x144',
           type: 'image/png'
         },
         {
-          src: 'images/touch/homescreen168.png',
+          src: '/orb168.png',
           sizes: '168x168',
           type: 'image/png'
         },
         {
-          src: 'images/touch/homescreen192.png',
+          src: '/orb192.png',
           sizes: '192x192',
           type: 'image/png'
-        }
-      ],
-      related_applications: [
-        {
-          platform: 'play',
-          url: 'https://play.google.com/store/apps/details?id=cheeaun.hackerweb'
         }
       ]
     })
