c4e48305536185499c3ab549bedebcbc4f7e64a5
Daniel Doblado <danieldoblado@gmail.com>
2023-08-13 01:25:37 +0200

    Full content RSS

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,6 +2,11 @@ import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
 import { SITE_TITLE, SITE_DESCRIPTION } from '#astro/consts'
 
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
+
+
 export async function get (context) {
   const posts = await getCollection('blog')
 
@@ -11,7 +16,8 @@ export async function get (context) {
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
-      link: `/blog/${post.slug}/`
+      link: `/blog/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body))
     }))
   })
 }
