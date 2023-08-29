9b6b50d76623ba3486111819ac4d787da004ddfb
Daniel Doblado <danieldoblado@gmail.com>
2023-08-29 15:24:46 +0200

    Improve rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,6 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
-import { SITE_TITLE, SITE_DESCRIPTION } from '#astro/consts'
+import { SITE_TITLE, SITE_DESCRIPTION, SITE_AUTHOR } from '#astro/consts'
 
 import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
@@ -13,6 +13,13 @@ export async function get (context) {
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
+    customData: `
+      <language>en</language>
+      <managingEditor>danieldoblado@gmail.com (${SITE_AUTHOR})</managingEditor>
+      <webMaster>danieldoblado@gmail.com (${SITE_AUTHOR})</webMaster>
+      <lastBuildDate>${(new Date()).toUTCString()}</lastBuildDate>
+      <generator>Astro</generator>
+    `,
     items: posts
       .filter((post) => !post.data.draft)
       .map((post) => {
@@ -21,7 +28,9 @@ export async function get (context) {
           link: `/blog/${post.slug}/`,
           content: sanitizeHtml(parser.render(post.body), {
             allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img' ])
-          })
+          }),
+          categories: post.data.tags,
+          author: SITE_AUTHOR
     }})
   })
 }
