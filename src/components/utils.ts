fc643fac2f9d6e64e60edd23cb77a7c0949c343b
Daniel Doblado <danieldoblado@gmail.com>
2023-09-13 09:22:07 +0200

    Updates

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,28 +1,33 @@
 export const getPostsPerYear = (posts) => {
-    return posts.reduce((posts, post) => {
+  return posts.reduce((posts, post) => {
+    if (post.data.draft === true) return posts;
 
-        if (post.data.draft === true) return posts;
+    const year = post.data.pubDate.getFullYear() || "Unknown";
 
-        const year = post.data.pubDate.getFullYear() || 'Unknown'
-    
-        if (posts[year] === undefined) {
-            posts[year] = [post];
-        } else {
-            posts[year].push(post);	
-        }
-        
-        return posts
-    }, {})
-}
+    if (posts[year] === undefined) {
+      posts[year] = [post];
+    } else {
+      posts[year].push(post);
+    }
 
-export const rand = (min: number, max: number) => {
-    return Math.random() * (max - min) + min;
+    return posts;
+  }, {});
 };
 
-export const map = (value: number, fromLow: number, fromHigh: number, toLow:number, toHigh:number) => {
-    const fromRange = fromHigh - fromLow;
-    const toRange = toHigh - toLow;
-    const scale = toRange / fromRange;
-    const mappedValue = (value - fromLow) * scale + toLow;
-    return mappedValue;
+export const rand = (min: number, max: number) => {
+  return Math.random() * (max - min) + min;
+};
+
+export const map = (
+  value: number,
+  fromLow: number,
+  fromHigh: number,
+  toLow: number,
+  toHigh: number,
+) => {
+  const fromRange = fromHigh - fromLow;
+  const toRange = toHigh - toLow;
+  const scale = toRange / fromRange;
+  const mappedValue = (value - fromLow) * scale + toLow;
+  return mappedValue;
 };
